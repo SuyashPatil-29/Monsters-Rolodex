ccae7a10bb41e6ce945beecbe0e9094da1ad3a91
SuyashPatil-29 <112413583+SuyashPatil-29@users.noreply.github.com>
2023-02-27 21:07:00 +0530

    used componentDidMount method to render names

--- src/Poke.jsx
+++ src/Poke.jsx
@@ -3,21 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 
-class App extends Component {
+class Poke extends Component {
   constructor() {
     super();
     
     this.state = {
-      monsters: [],
+      pokemons: [],
     }
 
   }
 
    componentDidMount(){
-     fetch('https://jsonplaceholder.typicode.com/users')
+     fetch('https://pokeapi.co/api/v2/pokemon')
        .then(response => response.json())
-       .then((users)=> this.setState(()=>{
-        return {monsters: users}
+       .then((poke)=> this.setState(()=>{
+        return {pokemons: poke.results}
        },
        ()=>{
         console.log(this.state);
@@ -27,12 +27,14 @@ class App extends Component {
   render(){
   return (
     <div className="App">
-      {this.state.monsters.map((monster)=>{
-        return <h1 key={monster.name}>{monster.name}</h1>
+      {this.state.pokemons.map((pokemon)=>{
+        return <div>
+        <h1 key={pokemon.name}>{pokemon.name}</h1>
+        </div>
       })}
     </div>
   );
 }
 }
 
-export default App;
+export default Poke;
